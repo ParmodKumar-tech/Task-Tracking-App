cc685371266f77bfe096078a3b815156af6536c4
ParmodKumar-tech <parmod1feb2002@gmail.com>
2025-04-19 20:43:02 +0530

    nav responsive

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -15,71 +15,64 @@ function Navbar(){
     }
 
 
-    let handleMenuItems=()=>{
+    const handleMenuItems=()=>{
         setMenuItems(!menuItems);
     }
 
+    const handleAutoCloseMenu=()=>{
+        setMenuItems(false);
+    }
     return(
-        <header className='w-full flex items-center gap-2 p-2 m-2 z-10'>
-        <div className='m-2 p-1 mx-auto flex items-center'>
+        <header className='mb-2 w-screen bg-green-300 flex items-center gap-2 p-2 z-10'>
         
+        <div className='w-[50%] mx-2 p-2 flex items-center'>
+    
         <div className='p-1 mx-2 h-fit rounded items-center justify-center bg-pink-200'>
         <h1 className='text-2xl'>TTA</h1>
         </div>
 
-        <div className='flex flex-col w-full'>
+        <div className='flex flex-col'>
         <Link to="/"><h2 className='font-bold sm:text-xl lg:text-xl'>Task Tracking App</h2></Link>
         <p className='hidden lg:block'>{userName?'Welcome Back!':'please login/register for view your tasks!'}</p>
         </div>
         
         </div>
 
-        <nav className='mx-auto flex h-fit items-center justify-center'>
-            <ul className='flex sm:visible md:flex-row invisible w-fit'>
-                {userName?
-                <>
-                <li className='font-bold'> <NavLink to="/" state={{ showAddTask: true }}>Add Task</NavLink></li>
+        <nav className='w-[50%] flex md:flex-row md:h-fit items-center justify-center'>
+            <ul className={menuItems?'w-screen left-0 bg-green-300 flex flex-col items-start absolute h-auto  border top-[13%]':
+                'hidden md:flex'}>
+                {userName &&
+                <div className='flex flex-col sm:flex-row p-2 gap-7 items-start '>
+                <li onClick={handleAutoCloseMenu} className='font-bold'> <NavLink to="/" state={{ showAddTask: true }}>Add Task</NavLink></li>
                 <li onClick={handleLogout} className='mx-2 font-bold'><NavLink to="/login">Logout</NavLink></li>
-                </>
-                :
+                </div>
+                }
+
+                {!userName &&
                 <li className='rounded-2xl p-1 bg-pink-600 text-white font-semibold'>
                     <NavLink to="/register">Login/Register</NavLink></li>
                 
                 }
                     
+                {
+                <div className='hidden md:flex'>    
                 <p className='separator my-auto font-bold mx-1'>|</p>
                 <p className='username align-middle my-auto mx-2'>
                 {userName?userName:"Not login"}</p>
-
+                </div>
+                }
             </ul>
-        
             
-            <div 
-            className="sm:hidden flex flex-col gap-1 p-2 cursor-pointer bg-pink-200 rounded" 
+        </nav>
+        
+        <div 
+            className="w-auto mr-5 md:hidden lg:hidden flex flex-col  gap-1 p-2 cursor-pointer bg-pink-200 rounded" 
             onClick={handleMenuItems}
             >
             <div className="w-6 h-0.5 bg-black"></div>
             <div className="w-6 h-0.5 bg-black"></div>
             <div className="w-6 h-0.5 bg-black"></div>
             </div>
-        
-        {menuItems && 
-        <div className='flex flex-wrap absolute w-fit'> 
-            <ul className='flex items-center justify-center bg-pink-100 my-2 '>
-            {userName?<>
-                <li className='font-bold my-2'> <NavLink to="/" state={{ showAddTask: true }}>Add Task</NavLink></li>
-                <li onClick={handleLogout} className='mx-2 font-bold'><NavLink to="/login">Logout</NavLink></li>
-                </>
-                :
-                <li className='rounded-2xl p-1 bg-pink-600 text-white font-semibold'>
-                <NavLink to="/register">Login/Register</NavLink></li>
-                }
-        </ul>
-        </div>}
-        
-        
-        </nav>
-    
        </header>
 
     )
