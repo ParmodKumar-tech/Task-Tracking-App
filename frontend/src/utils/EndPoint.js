4cc8c093f74f027c54ed8eae0ac7a73528244005
Parmod Kumar <parmod1feb2002@gmail.com>
2025-05-03 22:46:21 +0530

    Update EndPoint.js

--- frontend/src/utils/EndPoint.js
+++ frontend/src/utils/EndPoint.js
@@ -1,2 +1,2 @@
-export const USER_API_END_POINT="https://task-tracking-app-backend.vercel.app/user";
-export const TASK_API_END_POINT="https://task-tracking-app-backend.vercel.app/task";
+export const USER_API_END_POINT="https://task-tracking-app-backend.vercel.app/api/v1/user";
+export const TASK_API_END_POINT="https://task-tracking-app-backend.vercel.app/api/v1/task";
