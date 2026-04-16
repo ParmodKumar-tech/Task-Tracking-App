1ce96e10da44b4dcb9229288f67826587e26a322
Parmod Kumar <parmod1feb2002@gmail.com>
2026-04-16 13:57:05 +0530

    Update authUser.middleware.js

--- backend/src/middlewares/authUser.middleware.js
+++ backend/src/middlewares/authUser.middleware.js
@@ -11,7 +11,7 @@ export const validUser=async(req,res,next)=>{
     }
     
     try{
-        const token = authCookies.authToken;
+        const token = authCookies;
         const decode=jwt.verify(token,process.env.JWT_SECRET);
         req.userId=decode.id;
         next();
